417cca52f5fdb6867fba518f6a79fd9320264d27
Aluon <paulacanoroman@gmail.com>
2023-12-30 09:43:17 +0100

    feat: style title header

--- ScreenPrincipal.jsx
+++ ScreenPrincipal.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Task1 from './components/task1/Task1'
+import Header from './components/header/Header'
 
 const ScreenPrincipal = () => {
   return (
     <div>
+        <Header></Header>
         <Task1></Task1>
     </div>
   )
